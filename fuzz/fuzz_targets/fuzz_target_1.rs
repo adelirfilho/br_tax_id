6a7170f5a76dc75a1d813e125813286a93b53eb4
Adelir <github.boots307@passinbox.com>
2026-08-15 17:40:44 -0300

    refactor: rename variable texto_aleatorio to random_text in fuzz target for clarity

--- fuzz/fuzz_targets/fuzz_target_1.rs
+++ fuzz/fuzz_targets/fuzz_target_1.rs
@@ -4,10 +4,10 @@ use br_tax_id::validate_tax_id; // Importing my function
 
 fuzz_target!(|data: &[u8]| {
     // Attempts to convert the random bytes into a UTF-8 string
-    if let Ok(texto_aleatorio) = std::str::from_utf8(data) {
+    if let Ok(random_text) = std::str::from_utf8(data) {
         // Throw the text at your validator!
         // We don't care about Ok() or Err() here.
         // Our only goal is to ensure that this function NEVER causes a "panic!".
-        let _ = validate_tax_id(texto_aleatorio);
+        let _ = validate_tax_id(random_text);
     }
 });
